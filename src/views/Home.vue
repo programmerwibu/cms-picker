b89cf52b8136d52922de19bbeea1e730180c2a34
Ramanda Maulana <programmerwibu@gmail.com>
2021-11-22 15:23:14 +0700

    cms

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar />
+    <main class="content">
+      <Navbar />
+
+      <div class="container-fluid px-3 mt-5">
+        <div class="row mt-4 mb-3">
+          <div class="col-6 d-flex justify-content-between ps-0">
+            <div class="me-lg-3">
+              <a href="#" style="font-weight: bold; font-size: 36px">
+                List of Users</a
+              >
+            </div>
+          </div>
+          <div class="col-6 px-0 text-end">
+            <div class="btn-group">
+              <button class="btn btn-primary text-white">
+                Create New User
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Sidebar from "../components/navigation/Sidebar.vue";
+import Navbar from "../components/navigation/Navbar.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Sidebar,
+    Navbar,
+  },
+};
 </script>
+
+<style scoped>
+.btn-primary {
+  background: linear-gradient(180deg, #4790d9 0%, #0f75bc 100%) !important;
+  border: none;
+}
+</style>
